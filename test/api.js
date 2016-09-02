1a68ea789add47f5e38e2a9b22166d3fa22d0f83
Kevin Sawicki <kevinsawicki@gmail.com>
2016-09-02 11:02:08 -0700

    Add initial test

---
+++ test/api.js
@@ -0,0 +1,15 @@
+var assert = require('assert')
+var fs = require('fs')
+var getIconForExtension = require('..').getIconForExtension
+var path = require('path')
+
+var mp3Image = fs.readFileSync(path.join(__dirname, 'fixtures', 'mp3.png'))
+
+describe('getIconForExtension', function () {
+  it('calls back with a buffer', function (done) {
+    getIconForExtension('.mp3', function (error, buffer) {
+      assert.ok(mp3Image.equals(buffer))
+      done()
+    })
+  })
+})
