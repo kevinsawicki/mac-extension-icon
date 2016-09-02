ac02ec9103c5efee411795b2827936884e37e65b
Kevin Sawicki <kevinsawicki@gmail.com>
2016-09-02 11:24:15 -0700

    Assert error is null

--- test/api.js
+++ test/api.js
@@ -8,6 +8,7 @@ var mp3Image = fs.readFileSync(path.join(__dirname, 'fixtures', 'mp3.png'))
 describe('getIconForExtension', function () {
   it('calls back with a buffer', function (done) {
     getIconForExtension('.mp3', function (error, buffer) {
+      assert.equal(error, null)
       assert.ok(mp3Image.equals(buffer))
       done()
     })
